91ab300d4ad9e9d2b60510e5430bf47ee4cc4cac
Teo <lunasteobaldo@gmail.com>
2022-07-17 00:19:34 -0400

    Se crea archivo config para diponibilizar variables de entorno

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
-app.listen(port, () => `Api corriendo en puerto -- ${port}`)
+app.listen(port, () => console.log(`Api corriendo en puerto -- ${port}`))
